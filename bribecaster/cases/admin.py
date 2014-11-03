8d1ae90eb6a2cc6c83aeca0bf493219503e41ff9
Max Wolffe <max.alan.wolffe@gmail.com>
2014-11-03 09:22:40 -0800

    get admin functioning

--- bribecaster/cases/admin.py
+++ bribecaster/cases/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from cases.models import Citizen, Region, User, Office, Case, OfficeVisit, RoboCallFeedback, SMSFeedback 
 
 # Register your models here.
+admin.site.register(Citizen)
+admin.site.register(Region)
+admin.site.register(User)
+admin.site.register(Office)
+admin.site.register(Case)
+admin.site.register(OfficeVisit)
+admin.site.register(RoboCallFeedback)
+admin.site.register(SMSFeedback)
