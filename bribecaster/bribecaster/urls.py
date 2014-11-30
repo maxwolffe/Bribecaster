afe79298d716674080801f0ee9597476e3d3e181
Jeff Zheng <jeff.zheng.6@gmail.com>
2014-11-30 15:38:10 -0600

    Revert 783327a..358b382
    This rolls back to commit 783327aa8d5cbe76b1152378a95eb2ef0c1d7ba4.

--- bribecaster/bribecaster/urls.py
+++ bribecaster/bribecaster/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+dajaxice_autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bribecaster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index.html/', views.index, name='index'),
     url(r'^datatables.html/$', views.table, name='table'),
@@ -22,9 +26,7 @@ urlpatterns = patterns('',
     url(r'^cases/new_form/aadhaar_lookup/$', views.aadhaar_lookup, name='aadhaar_lookup'),
     url(r'^cases/obc_form/ci-(?P<citizen_id>\d+)$', views.obc_form, name='obc_form_ci'),
     url(r'^cases/obc_form/an-(?P<aadhaar_number>\d+)$', views.obc_form, name='obc_form_an'),
-    url(r'^cases/obc_form/$', views.obc_form, name='obc_form'),
-    url(r'^cases/office-charts/$', views.office_chart, name='office-charts'),
-    url(r'^cases/office-charts/(?P<office_id>\d+)$', views.office_chart, name='office-charts')
+    url(r'^cases/obc_form/$', views.obc_form, name='obc_form')
 )
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
