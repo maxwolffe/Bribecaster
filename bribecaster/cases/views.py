783327aa8d5cbe76b1152378a95eb2ef0c1d7ba4
Jeff Zheng <jeff.zheng.6@gmail.com>
2014-11-30 15:02:17 -0600

    added line charts for offices

--- bribecaster/cases/views.py
+++ bribecaster/cases/views.py
@@ -4,27 +4,16 @@ from models import Citizen, OBCFormResponse, Case, Office, OfficeVisit
 from forms import CaseForm, OBCFormForm, CitizenForm, AadhaarLookup, Form
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from datetime import date
 
 def index(request):
     return render_to_response('bribecaster/index.html', context_instance=RequestContext(request))
 
-def form(request):
-    if request.method == 'POST':
-        pass
-        # create a form instance and populate it with data from the request:
-        form = Form(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            form.save()
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('bribecaster/form-showcase.html')
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = Form()
-    return render(request, 'bribecaster/form-showcase.html', {'form': form})
+def casesView(request):
+    current_month = date.today().month;
+    context = {'month': current_month};
+    return render(request, 'bribecaster/casesView.html', context); 
 
 def detail(request, case_id):
     if request.method == "GET":
