aa6ae33c8a5eb00002a82df47be19268d80b087d
Max Wolffe <max.alan.wolffe@gmail.com>
2014-11-30 22:02:37 -0800

    fix syntax errors and html probs

--- bribecaster/urls.py
+++ bribecaster/urls.py
@@ -1,4 +1,4 @@
-fom cases import views
+from cases import views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
@@ -12,14 +12,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'bribecaster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^index.html/', views.index, name='index'),
+    url(r'^index/', views.index, name='index'),
     url(r'^datatables/$', views.table, name='table'),
     url(r'^datatables/sms-only$', views.SMSOnlyTable, name='smstable'),
     url(r'^datatables/robo-only$', views.RobocallOnlyTable, name='robotable'),
     url(r'^datatables/(?P<case_id>\d+)/$', views.detail, name='detail'),
     url(r'^$', views.index, name='index'),
     url(r'^reports/$', views.casesView, name='casesView'),
-    url(r'^charts/pieCharts.html/$', views.pieCharts, name='pieCharts'),
+    url(r'^charts/pieCharts/$', views.pieCharts, name='pieCharts'),
     url(r'^cases/new_form/aadhaar_lookup/$', views.aadhaar_lookup, name='aadhaar_lookup'),
     url(r'^cases/obc_form/ci-(?P<citizen_id>\d+)$', views.obc_form, name='obc_form_ci'),
     url(r'^cases/obc_form/an-(?P<aadhaar_number>\d+)$', views.obc_form, name='obc_form_an'),
